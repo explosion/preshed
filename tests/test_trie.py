dcd6bf03160a7befa76b92b0027d73373f3e0510
Matthew Honnibal <honnibal@gmail.com>
2014-10-05 00:31:08 +1000

    * Fix insertion to place after last seen key

--- tests/test_trie.py
+++ tests/test_trie.py
@@ -26,3 +26,36 @@ def test_len1(tree):
     assert tree[(5,)] == 20
     assert tree.first == 25
     assert tree.last == 20
+    tree[(7,)] = 30
+    assert tree[(7,)] == 30
+    assert tree[(2,)] == 25
+    assert tree[(5,)] == 20
+    assert tree.first == 25
+    assert tree.last == 30
+ 
+
+def test_len2(tree):
+    assert tree[(0, 0)] == None
+    tree[(0, 0)] = 1
+    assert tree[(0, 0)] == 1
+    tree[(0, 1)] = 2
+    assert tree[(0, 0)] == 1
+    assert tree[(0, 1)] == 2
+    assert tree[(1, 0)] == None
+    tree[(1, 0)] = 3
+    assert tree[(1, 0)] == 3
+    tree[(1, 1)] = 4
+    assert tree[(1, 1)] == 4
+
+    assert tree[(0, 0)] == 1
+    assert tree[(0, 1)] == 2
+    assert tree[(1, 0)] == 3
+
+    assert tree[(0,)] == None
+    tree[(0,)] = 5
+    assert tree[(0,)] == 5
+
+    assert tree[(0, 0)] == 1
+    assert tree[(0, 1)] == 2
+    assert tree[(1, 0)] == 3
+    assert tree[(1, 1)] == 4
