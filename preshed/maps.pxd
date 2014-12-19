acd3feb78041223e1fa9ed3fb223b097c5c5ebbe
Matthew Honnibal <honnibal@gmail.com>
2014-12-19 22:31:05 +1100

    * Fix handling of 0 keys: can now permit 0 as a valid key.

--- preshed/maps.pxd
+++ preshed/maps.pxd
@@ -1,5 +1,3 @@
-cimport cython
-
 from libc.stdint cimport uint64_t
 from cymem.cymem cimport Pool
 
@@ -13,23 +11,16 @@ cdef struct Cell:
 
 
 cdef struct MapStruct:
+    Cell* cells
+    void* value_for_empty_key
+    void* value_for_del_key
     size_t length
     size_t filled
-    Cell* cells
+    bint is_empty_key_set
+    bint is_del_key_set
 
 
-@cython.cdivision
-cdef inline Cell* _find_cell(const Cell* cells, const size_t size, const key_t key) nogil:
-    # Modulo for powers-of-two via bitwise &
-    cdef size_t i = (key & (size - 1))
-    while cells[i].key != 0 and cells[i].key != key:
-        i = (i + 1) & (size - 1)
-    return &cells[i]
-
-cdef inline void* map_get(const MapStruct* map_, const key_t key) nogil:
-    cdef Cell* cell = _find_cell(map_.cells, map_.length, key)
-    return cell.value
-
+cdef void* map_get(const MapStruct* map_, const key_t key) nogil
 
 cdef void map_set(Pool mem, MapStruct* map_, key_t key, void* value) except *
 
--- preshed/maps.pyx
+++ preshed/maps.pyx
@@ -1,5 +1,9 @@
 # cython: profile=True
+cimport cython
 
+
+DEF EMPTY_KEY = 0
+DEF DELETED_KEY = 1
 
 cdef class PreshMap:
     """Hash map that assumes keys come pre-hashed. Uses open addressing with
@@ -7,8 +11,13 @@ cdef class PreshMap:
     """
     def __init__(self, size_t initial_size=8):
         # Size must be power of two
-        assert initial_size != 0
-        assert initial_size & (initial_size - 1) == 0
+        if initial_size == 0:
+            initial_size = 8
+        if initial_size & (initial_size - 1) != 0:
+            power = 1
+            while power < initial_size:
+                power *= 2
+            initial_size = power
         self.mem = Pool()
         self.c_map = <MapStruct*>self.mem.alloc(1, sizeof(MapStruct))
         map_init(self.mem, self.c_map, initial_size)
@@ -58,14 +67,38 @@ cdef void map_init(Pool mem, MapStruct* map_, size_t length) except *:
 
 
 cdef void map_set(Pool mem, MapStruct* map_, key_t key, void* value) except *:
+    if key == EMPTY_KEY:
+        map_.value_for_empty_key = value
+        map_.is_empty_key_set = True
+    elif key == DELETED_KEY:
+        map_.value_for_del_key = value
+        map_.is_del_key_set = True
     cdef Cell* cell
     cell = _find_cell(map_.cells, map_.length, key)
-    if cell.key == 0:
+    if cell.key == EMPTY_KEY:
         cell.key = key
         map_.filled += 1
     cell.value = value
     if (map_.filled + 1) * 4 >= (map_.length * 3):
         _resize(mem, map_)
+
+
+cdef void* map_get(const MapStruct* map_, const key_t key) nogil:
+    if key == EMPTY_KEY:
+        return map_.value_for_empty_key
+    elif key == DELETED_KEY:
+        return map_.value_for_del_key
+    cdef Cell* cell = _find_cell(map_.cells, map_.length, key)
+    return cell.value
+
+
+@cython.cdivision
+cdef inline Cell* _find_cell(const Cell* cells, const size_t size, const key_t key) nogil:
+    # Modulo for powers-of-two via bitwise &
+    cdef size_t i = (key & (size - 1))
+    while cells[i].key != 0 and cells[i].key != key:
+        i = (i + 1) & (size - 1)
+    return &cells[i]
 
 
 cdef void _resize(Pool mem, MapStruct* map_) except *:
@@ -80,8 +113,7 @@ cdef void _resize(Pool mem, MapStruct* map_) except *:
     cdef size_t i
     cdef size_t slot
     for i in range(old_size):
-        if old_cells[i].key != 0:
-            assert old_cells[i].value != NULL, i
+        if old_cells[i].key != EMPTY_KEY and old_cells[i].key != DELETED_KEY:
             map_set(mem, map_, old_cells[i].key, old_cells[i].value)
     mem.free(old_cells)
 
