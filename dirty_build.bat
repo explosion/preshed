6312474d6820f78cdef189da2cbc3cd39b01a0fe
maxirmx <m.samsonov@ieee.org>
2015-10-09 14:53:05 +0300

    Windows-safe version
    This change provides for Windows setup but probably does not help to do
    what has been intended originally.

---
+++ dirty_build.bat
@@ -0,0 +1,3 @@
+SET INCLUDE=C:\Users\maxim_000\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\WinSDK\Include\
+@echo %INCLUDE% 
+python setup.py build
--- setup.py
+++ setup.py
@@ -19,9 +19,10 @@ distutils.sysconfig.get_config_vars()
 
 def rm_cflag(text):
     if distutils.sysconfig._config_vars:
-        cflags = distutils.sysconfig._config_vars['CFLAGS']
-        cflags = cflags.replace(text, '')
-        distutils.sysconfig._config_vars['CFLAGS'] = cflags
+	cflags = distutils.sysconfig.get_config_var('CFLAGS') 
+        if cflags: 
+        	cflags = cflags.replace(text, '')
+        	distutils.sysconfig._config_vars['CFLAGS'] = cflags
 
 
 includes = ['.', path.join(sys.prefix, 'include')]
