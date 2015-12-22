2f46b21826b2b058da116650a16aab34cdee26ad
Henning Peters <pete@dexterslab.de>
2015-12-22 15:03:12 +0100

    set up buildbot scripts

---
+++ venv.ps1
@@ -0,0 +1,18 @@
+param (
+    [string]$python = $(throw "-python is required."),
+    [string]$install_mode = $(throw "-install_mode is required."),
+    [string]$pip_date
+)
+
+$ErrorActionPreference = "Stop"
+ 
+if(!(Test-Path -Path ".build"))
+{
+    virtualenv .build --python $python
+}
+.build\Scripts\activate.ps1
+
+python build.py prepare $pip_date
+python build.py $install_mode
+python build.py test
+exit $LASTEXITCODE
