8bc067b64a16c25aaaa841170ea327ebdac98251
Henning Peters <pete@dexterslab.de>
2016-02-10 20:45:08 +0100

    sync setup.py/cythonize with spacy

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import division, print_function
+from __future__ import print_function
 import os
 import shutil
 import subprocess
@@ -14,15 +14,12 @@ except ImportError:
     from distutils.core import Extension, setup
 
 
-MAJOR      = 0
-MINOR      = 46
-MICRO      = 1
-ISRELEASED = True
-VERSION    = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-
-
-PACKAGES = ['preshed', 'preshed.tests']
-MOD_NAMES = ['preshed.maps', 'preshed.counter']
+PACKAGES = [
+    'preshed',
+    'preshed.tests']
+MOD_NAMES = [
+    'preshed.maps',
+    'preshed.counter']
 
 
 # By subclassing build_extensions we have the actual compiler that will be used which is really known only after finalize_options
@@ -31,12 +28,6 @@ compile_options =  {'msvc'  : ['/Ox', '/EHsc'],
                     'other' : ['-O3', '-Wno-strict-prototypes', '-Wno-unused-function']}
 link_options    =  {'msvc'  : [],
                     'other' : []}
-
-if sys.platform.startswith('darwin'):
-    compile_options['other'].append('-mmacosx-version-min=10.8')
-    compile_options['other'].append('-stdlib=libc++')
-    link_options['other'].append('-lc++')
-
 
 class build_ext_options:
     def build_options(self):
@@ -52,73 +43,6 @@ class build_ext_subclass(build_ext, build_ext_options):
     def build_extensions(self):
         build_ext_options.build_options(self)
         build_ext.build_extensions(self)
-
-
-# Return the git revision as a string
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = 'Unknown'
-
-    return GIT_REVISION
-
-
-def get_version_info():
-    # Adding the git rev number needs to be done inside write_version_py(),
-    # otherwise the import of preshed.about messes up the build under Python 3.
-    FULLVERSION = VERSION
-    if os.path.exists('.git'):
-        GIT_REVISION = git_version()
-    elif os.path.exists(os.path.join('preshed', 'about.py')):
-        # must be a source distribution, use existing version file
-        try:
-            from preshed.about import git_revision as GIT_REVISION
-        except ImportError:
-            raise ImportError('Unable to import git_revision. Try removing '
-                              'preshed/about.py and the build directory '
-                              'before building.')
-    else:
-        GIT_REVISION = 'Unknown'
-
-    if not ISRELEASED:
-        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
-
-    return FULLVERSION, GIT_REVISION
-
-
-def write_version(path):
-    cnt = """# THIS FILE IS GENERATED FROM SETUP.PY
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-git_revision = '%(git_revision)s'
-release = %(isrelease)s
-if not release:
-    version = full_version
-"""
-    FULLVERSION, GIT_REVISION = get_version_info()
-
-    with open(path, 'w') as f:
-        f.write(cnt % {'version': VERSION,
-                       'full_version' : FULLVERSION,
-                       'git_revision' : GIT_REVISION,
-                       'isrelease': str(ISRELEASED)})
 
 
 def generate_cython(root, source):
@@ -162,7 +86,9 @@ def setup_package():
         return clean(root)
 
     with chdir(root):
-        write_version(os.path.join(root, 'preshed', 'about.py'))
+        about = {}
+        with open(os.path.join(root, 'preshed', 'about.py')) as f:
+            exec(f.read(), about)
 
         include_dirs = [
             get_python_inc(plat_specific=True),
@@ -179,16 +105,15 @@ def setup_package():
             generate_cython(root, 'preshed')
 
         setup(
-            name='preshed',
+            name=about['__name__'],
             packages=PACKAGES,
-            package_data={'preshed': ['*.pyx',
-                                      '*.pxd']},
-            description='Cython hash table that trusts the keys are pre-hashed',
-            author='Matthew Honnibal',
-            author_email='matt@spacy.io',
-            version=VERSION,
-            url='http://github.com/syllog1sm/preshed',
-            license='MIT',
+            package_data={'': ['*.pyx', '*.pxd']},
+            description=about['__summary__'],
+            author=about['__author__'],
+            author_email=about['__email__'],
+            version=about['__version__'],
+            url=about['__uri__'],
+            license=about['__license__'],
             ext_modules=ext_modules,
             classifiers=['Environment :: Console',
                          'Operating System :: OS Independent',
