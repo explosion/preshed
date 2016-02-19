b32f56f0d52efbfccffe5ee84adb140020577fa7
Matthew Honnibal <honnibal+gh@gmail.com>
2016-02-19 15:16:47 +0100

    * Increment version

--- preshed/about.py
+++ preshed/about.py
@@ -4,10 +4,10 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 
 __name__ = 'preshed'
-__version__ = '0.46.1'
+__version__ = '0.46.2'
 __summary__ = 'Cython hash table that trusts the keys are pre-hashed'
 __uri__ = 'http://github.com/syllog1sm/preshed'
 __author__ = 'Matthew Honnibal'
 __email__ = 'matt@spacy.io'
 __license__ = 'MIT'
-__release__ = False
+__release__ = True
