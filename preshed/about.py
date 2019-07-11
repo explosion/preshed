a91dbc41f8e29db782a5be14a7daf92a580f9bef
svlandeg <sofie.vanlandeghem@gmail.com>
2019-07-11 11:31:51 +0200

    bump version to 2.0.2

--- preshed/about.py
+++ preshed/about.py
@@ -1,5 +1,5 @@
 __title__ = "preshed"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __summary__ = "Cython hash table that trusts the keys are pre-hashed"
 __uri__ = "https://github.com/explosion/preshed"
 __author__ = "Matthew Honnibal"
