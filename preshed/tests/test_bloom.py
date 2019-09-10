07e6a6c278293b8c81d447ae18f1282d2e10f1c8
Matthew Honnibal <honnibal+gh@gmail.com>
2019-09-10 18:40:05 +0200

    Support pickle for bloom filter

--- preshed/tests/test_bloom.py
+++ preshed/tests/test_bloom.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import pytest
+import pickle
 
 from preshed.bloom import BloomFilter
 
@@ -44,3 +45,12 @@ def test_to_from_bytes():
     for ii in range(0,1000,20):
         assert ii in bf2
     assert bf2.to_bytes() == data
+
+def test_bloom_pickle():
+    bf = BloomFilter(size=100, hash_funcs=2)
+    for ii in range(0,1000,20):
+        bf.add(ii)
+    data = pickle.dumps(bf)
+    bf2 = pickle.loads(data)
+    for ii in range(0,1000,20):
+        assert ii in bf2
