db17f89ca616ced4854f57eea02e99af93a14a23
Matthew Honnibal <honnibal+gh@gmail.com>
2020-11-18 17:17:21 +0800

    Upd fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,8 @@
-from fabric.api import local, run, lcd, cd, env
+from fabric.api import local, run, lcd, cd, env, settings
 
 import os
 from os import path
 from os.path import exists as file_exists
-from fabtools.python import virtualenv
 
 
 PWD = path.dirname(__file__)
@@ -11,60 +10,6 @@ VENV_DIR = path.join(PWD, '.env')
 DEV_ENV_DIR = path.join(PWD, '.denv')
 
 
-def dev():
-    # Allow this to persist, since we aren't as rigorous about keeping state clean
-    if not file_exists('.denv'):
-        local('virtualenv .denv')
- 
-    with virtualenv(DEV_ENV_DIR):
-        local('pip install -r requirements.txt')
- 
-
-def sdist():
-    if file_exists('dist/'):
-        local('rm -rf dist/')
-    local('mkdir dist')
-    with virtualenv(VENV_DIR):
-        local('python setup.py sdist')
-
-
-def publish():
-    with virtualenv(VENV_DIR):
-        local('python setup.py register')
-        local('twine upload dist/*.tar.gz')
-
-
-def setup():
-    if file_exists('.env'):
-        local('rm -rf .env')
-    local('rm -rf *.egg')
-    local('virtualenv .env')
-
-
-def install():
-    with virtualenv(VENV_DIR):
-        local('pip install --upgrade setuptools')
-        local('pip install dist/*.tar.gz')
-        local('pip install pytest')
-
-
-def make():
-    with virtualenv(DEV_ENV_DIR):
-        with lcd(path.dirname(__file__)):
-            local('python setup.py build')
-
-
-def clean():
-    with lcd(os.path.dirname(__file__)):
-        local('python setup.py clean --all')
-    with virtualenv(DEV_ENV_DIR):
-        with lcd(os.path.dirname(__file__)):
-            local('python setup.py clean --all')
-
-def test():
-    with virtualenv(VENV_DIR):
-        local('python -m pytest -x')
-
-
-def travis():
-    local('open https://travis-ci.org/spacy-io/preshed')
+def login():
+    with settings(user="matt"):
+        run("ls")
