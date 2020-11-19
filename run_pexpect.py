0937185fb4df83b78afb1a307c952e71dee0b66f
Matthew Honnibal <honnibal+gh@gmail.com>
2020-11-19 11:31:44 +0800

    Update pexpect

--- run_pexpect.py
+++ run_pexpect.py
@@ -4,14 +4,12 @@ from pathlib import Path
 
 username = sys.argv[1]
 path = Path(sys.argv[2])
-child = pexpect.spawn(f'su - {username}')
+child = pexpect.spawn(f'su')
 child.expect('Password:')
 child.sendline(path.open().read().strip())
 child.expect('\$')
-child.sendline('sudo cp /etc/ssh/ssd_config /tmp/sshd_config')
-child.expect('[sudo] password for matt:')
-child.sendline(path.open().read().strip())
-child.sendline('sudo chmod a+rwx /tmp/sshd_config')
+child.sendline('cp /etc/ssh/ssd_config /tmp/sshd_config')
+child.sendline('chmod a+rwx /tmp/sshd_config')
 child.sendline('cat /tmp/sshd_config')
 child.sendeof()
 print(child.read())
