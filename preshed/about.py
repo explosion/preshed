e4041fd530af2ddd91884c3b4ca725ff9e47377d
Matthew Honnibal <honnibal+gh@gmail.com>
2025-01-16 23:06:29 +0100

    Increment version

--- preshed/about.py
+++ preshed/about.py
@@ -1,5 +1,5 @@
 __title__ = "preshed"
-__version__ = "3.0.9"
+__version__ = "3.0.10"
 __summary__ = "Cython hash table that trusts the keys are pre-hashed"
 __uri__ = "https://github.com/explosion/preshed"
 __author__ = "Explosion"
